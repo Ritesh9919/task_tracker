a182d044c79e006c3c3a6d80f8fe0c6286a2520b
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-05-01 20:05:38 +0530

    fixes some bugs in cors optios

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -44,6 +44,7 @@ export const login = async(req, res, next)=> {
         res.cookie('token',token, {
             httpOnly:true,
             secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             expires:expiryDate,
             maxAge: 24 * 60 * 60 * 1000,
             path:'/',
