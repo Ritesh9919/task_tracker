fb3340474bcd88ad822b235f8310a3ce9d673766
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-05-01 19:29:59 +0530

    added origin in cors

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://task-tracker-12mern.netlify.app/',
     credentials:true,
     
     
