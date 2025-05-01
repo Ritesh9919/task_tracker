208439a85d4f625f431d685aaa8f1932966b7b99
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-05-01 20:23:21 +0530

    added env sample

--- src/app.js
+++ src/app.js
@@ -16,7 +16,8 @@ app.use(cors({
     origin:'https://task-tracker-12mern.netlify.app',
     credentials:true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    
     
     
 }))
